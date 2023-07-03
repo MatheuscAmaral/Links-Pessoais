0e76fc40c1aef50a861db3b3954fb7bce8087c14
Matheus Amaral <matheus.amaral.1443007@sga.pucminas.br>
2023-07-03 20:16:50 -0300

    fazendo os çinks pessoais

---
+++ Matheus Links/script.js
@@ -0,0 +1,13 @@
+function toogleMode() {
+  let html = document.documentElement
+
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  }
+
+  else {
+    html.classList.add('light')
+  }
+
+  
+}
